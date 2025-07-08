8c181ebd92857a09abbebc451a04c8c9ecfc545a
eman <eman.abdelgawada@gmail.com>
2025-07-08 12:20:51 +0300

    add comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
+//method to checkOddEven
 void checkOddEven(int num)
 {
+	//check if num is even
     if (num % 2 == 0) {
         std::cout << num << " is even." << std::endl;
     }
+	//else it is odd
     else {
         std::cout << num << " is odd." << std::endl;
     }
